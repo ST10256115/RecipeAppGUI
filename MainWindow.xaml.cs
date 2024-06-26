b3516573da4b10a28b649d020a92f37f712604e0
ShivarTuplah <ST10256115@vcconnect.edu.za>
2024-06-26 22:52:37 +0200

    removed unneccessary comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,9 +12,6 @@ using System.Windows.Shapes;
 
 namespace RecipeAppGUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
 
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
